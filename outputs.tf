2dc4ab194ece5bcbea40eb602b40ae604a248969
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-08-13 13:55:04 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "FoggyKitchenOKECluster" {
   value = {
-    id                 = oci_containerengine_cluster.FoggyKitchenOKECluster.id
-    kubernetes_version = oci_containerengine_cluster.FoggyKitchenOKECluster.kubernetes_version
-    name               = oci_containerengine_cluster.FoggyKitchenOKECluster.name
+    id                 = oci_containerengine_cluster.kubeOKECluster.id
+    kubernetes_version = oci_containerengine_cluster.kubeOKECluster.kubernetes_version
+    name               = oci_containerengine_cluster.kubeOKECluster.name
   }
 }
 
 output "FoggyKitchenOKENodePool" {
   value = {
-    id                 = oci_containerengine_node_pool.FoggyKitchenOKENodePool.id
-    kubernetes_version = oci_containerengine_node_pool.FoggyKitchenOKENodePool.kubernetes_version
-    name               = oci_containerengine_node_pool.FoggyKitchenOKENodePool.name
-    subnet_ids         = oci_containerengine_node_pool.FoggyKitchenOKENodePool.subnet_ids
+    id                 = oci_containerengine_node_pool.kubeOKENodePool.id
+    kubernetes_version = oci_containerengine_node_pool.kubeOKENodePool.kubernetes_version
+    name               = oci_containerengine_node_pool.kubeOKENodePool.name
+    subnet_ids         = oci_containerengine_node_pool.kubeOKENodePool.subnet_ids
   }
 }
 
 output "FoggyKitchen_Cluster_Kubernetes_Versions" {
-  value = [data.oci_containerengine_cluster_option.FoggyKitchenOKEClusterOption.kubernetes_versions]
+  value = [data.oci_containerengine_cluster_option.kubeOKEClusterOption.kubernetes_versions]
 }
 
 output "FoggyKitchen_Cluster_NodePool_Kubernetes_Version" {
-  value = [data.oci_containerengine_node_pool_option.FoggyKitchenOKEClusterNodePoolOption.kubernetes_versions]
+  value = [data.oci_containerengine_node_pool_option.kubeOKEClusterNodePoolOption.kubernetes_versions]
 }
