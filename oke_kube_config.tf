4b9bb529d563434ea20b1cfc07fc089de2a54b07
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-08-13 13:53:08 +0530

    Update oke_kube_config.tf

--- oke_kube_config.tf
+++ oke_kube_config.tf
@@ -2,14 +2,14 @@ variable "cluster_kube_config_token_version" {
   default = "2.0.0"
 }
 
-data "oci_containerengine_cluster_kube_config" "FoggyKitchenKubeConfig" {
-  cluster_id = oci_containerengine_cluster.FoggyKitchenOKECluster.id
+data "oci_containerengine_cluster_kube_config" "KubeConfig" {
+  cluster_id = oci_containerengine_cluster.kubeOKECluster.id
 
   #Optional
   token_version = var.cluster_kube_config_token_version
 }
 
-resource "local_file" "FoggyKitchenKubeConfigFile" {
-  content  = data.oci_containerengine_cluster_kube_config.FoggyKitchenKubeConfig.content
+resource "local_file" "KubeConfigFile" {
+  content  = data.oci_containerengine_cluster_kube_config.KubeConfig.content
   filename = "test_cluster_kubeconfig"
 }
